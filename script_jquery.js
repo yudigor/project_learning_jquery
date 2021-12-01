db22ca6f7ef7804a04ea52653593c51d08b2bf54
Igor Yudi <40796437+yudigor@users.noreply.github.com>
2021-11-30 22:34:49 -0300

    Trying to assign divs

--- script_jquery.js
+++ script_jquery.js
@@ -1,6 +1,9 @@
 $(function(){
-    $('button').click(function(){
-        $('h1').hide();
+    $('#vermelho').click(function(){
+        $('#1').css('background-color', 'red');
     });
 
+    $('#azul').click(function(){
+        $('#2').css('background-color', 'blue');
+    }); 
 });
